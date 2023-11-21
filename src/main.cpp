b59d20a4b99b7b0c1e1ebfaf449069124434b6fb
Sergi-Virgili <svirgilif@gmail.com>
2023-11-21 22:13:12 +0100

    Add LED blinking and serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <Arduino.h>
-
-int myFunction(int, int);
+#define LED_BUILTIN 13
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop()
 {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+  Serial.println("End of loop");
   // put your main code here, to run repeatedly:
 }
 
